9677e51a5d04212873d74d6a8b99f91944a5d994
Tigran <tig.vardanyan.001@gmail.com>
2020-05-30 21:20:24 +0400

    ready

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,13 @@
-/*
-scripts
- */
+const checkbox = document.querySelector('.slider');
+const input = document.querySelector('input');
+const smile_reye = document.querySelector('.smile_reye');
+const smile_leye = document.querySelector('.smile_leye');
+checkbox.addEventListener('click', ()=> {
+  if(input.checked) {
+    smile_leye.innerHTML = '<i class="fas fa-chevron-right"></i>';
+    smile_reye.innerHTML = '<i class="fas fa-chevron-left"></i>'
+  } else {
+    smile_leye.innerHTML = '<i class="fas fa-circle"></i>';
+    smile_reye.innerHTML = '<i class="fas fa-circle"></i>';;
+  }
+});
